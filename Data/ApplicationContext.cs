64c783fae28c9acc094ad8f46f0bd205a10c6eb3
Mikhail <mikhailArzumanov@yandex.ru>
2023-01-05 15:12:17 +0300

    Были добавлены gitignore и класс контекста приложения

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace publicationsApi.Data{
+    public class ApplicationContext : DbContext{
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {
+            //Database.EnsureCreated();
+        }
+        
+    }
+}
