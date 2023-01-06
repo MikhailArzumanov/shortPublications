0530690b7b291a7b4042dd718520e76c23d35a7f
Mikhail <mikhailArzumanov@yandex.ru>
2023-01-06 21:07:00 +0300

    Были осуществлены миграции добавления таблиц моделей

---
+++ Migrations/20230106180319_ModelsAddition.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace publicationsApi.Migrations{
+    public partial class ModelsAddition : Migration{
+        protected override void Up(MigrationBuilder migrationBuilder){
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    IsAdmin = table.Column<bool>(type: "boolean", nullable: false),
+                    WasBanned = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publications",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    SetTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    AuthorId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Publications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Publications_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commentaries",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    SetTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    AuthorId = table.Column<int>(type: "integer", nullable: true),
+                    PublicationId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Commentaries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commentaries_Publications_PublicationId",
+                        column: x => x.PublicationId,
+                        principalTable: "Publications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Commentaries_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commentaries_AuthorId",
+                table: "Commentaries",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commentaries_PublicationId",
+                table: "Commentaries",
+                column: "PublicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publications_AuthorId",
+                table: "Publications",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder){
+            migrationBuilder.DropTable(
+                name: "Commentaries");
+
+            migrationBuilder.DropTable(
+                name: "Publications");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
