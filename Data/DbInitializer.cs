9f4da674cc56fdd42c1f2d5b0c9f0f64fa01ebc0
Mikhail <48182639+MikhailArzumanov@users.noreply.github.com>
2023-01-06 17:28:46 +0300

    Заполнитель файла DbInitializer.cs был добавлен

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using publicationsApi.Models;
+
+namespace publicationsApi.Data{
+    public class DbInitializer{
+        public static void Initialize(ApplicationContext context){
+            //Добавьте изначальные данные в базу здесь
+        }
+    }
+}
