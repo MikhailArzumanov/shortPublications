b357818710b2842fd484f84b42a7eaefb7b39ba5
Mikhail <mikhailArzumanov@yandex.ru>
2023-01-09 08:52:40 +0300

    Были доработаны основная страница и основной компонент приложения

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import './App.css';
-import LoginForm from '../components/LoginForm/LoginForm';
+import PublicationForm from '../components/PublicationForm/PublicationForm';
+import { PublicationsService } from '../-services/publications.service';
+import PublicationBlock from '../components/PublicationBlock/PublicationBlock';
 
-function App() {
-  return (
-      <LoginForm />
-  );
+class App extends React.Component {
+
+  async render() {
+      let response = await PublicationsService.getList();
+      let publications = response.publications;
+      let publicationBlocks = publications.map((pub)=> <PublicationBlock text={pub.text}/>)
+      return (
+        <>
+          <PublicationForm />
+          <hr className='separator'/>
+          {publicationBlocks}
+        </>
+      );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import { AuthorizedService } from './-services/-base-services/authorized.service';
+import LoginForm from './components/LoginForm/LoginForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+if(AuthorizedService.isAuthorized()){
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+else{
+  root.render(
+    <React.StrictMode>
+      <LoginForm />
+    </React.StrictMode>
+  );
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
